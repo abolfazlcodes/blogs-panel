ec5381965e6f755ef4a4c181a70045b9b0e140a9
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-08-20 22:20:17 +0330

    feat: add basic text editor using tiptap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import BlogTextEditor from "./components/BlogTextEditor";
+
 function App() {
-  return <div className="border border-red-500">app</div>;
+  const [content, setContent] = useState("hello");
+
+  return (
+    <div className="border border-red-500 w-full p-4 min-h-svh">
+      <BlogTextEditor
+        content={content}
+        onChange={(editorContent) => setContent(editorContent)}
+      />
+    </div>
+  );
 }
 
 export default App;
