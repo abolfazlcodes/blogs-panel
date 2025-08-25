43030570c071f5ace5fb3ffbb578a3618a86c263
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-08-25 19:20:38 +0330

    feat: add blogs card - #5

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,19 @@
+import BlogCard from "@/components/BlogCard";
+
 const HomePage = () => {
-  return <main>Home page</main>;
+  return (
+    <main className="min-h-svh">
+      <header className="px-2 py-4 text-xl font-serif font-extrabold">
+        <h1>Here are your top stories</h1>
+      </header>
+
+      <section className="flex flex-wrap gap-6 p-2 my-10">
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
